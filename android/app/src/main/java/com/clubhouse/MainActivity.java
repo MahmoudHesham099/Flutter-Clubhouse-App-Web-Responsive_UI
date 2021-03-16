177c5c0c42b5b1e666739fb752b87a365ffdaa36
hesham <17p6020@eng.asu.edu.eg>
2021-03-16 21:40:52 +0200

    initial

---
+++ android/app/src/main/java/com/clubhouse/MainActivity.java
@@ -0,0 +1,6 @@
+package com.clubhouse;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:clubhouse/Screens/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
